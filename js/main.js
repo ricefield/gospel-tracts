41aca10e902b61b379ebe91d3417aec2f948aec5
Jonathan Tien <jonathan.tien@gmail.com>
2014-10-16 22:44:52 -0700

    more styling of mohl

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
     $(document).ready(function() {
-        $("#headline").prepend('<a href="../../"><i class="fa fa-chevron-left"></i>&nbsp;&nbsp;')
+        $("#headline").prepend('<a href="../../"><i class="fa fa-chevron-left pull-left"></i></a>')
     });
 
     $("p").click(function() {
